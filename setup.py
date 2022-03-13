d7648aeab9db427a93e4bbd48336ae9cb9810a81
RJ White <rj@moxad.com>
2022-03-13 09:46:30 -0400

    v0.7: added options -t and -s to send-sms-message

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup( 
     name                = "python-voip.ms",
-    version             = "0.6",
+    version             = "0.7",
     scripts             = [ 'scripts/black-list', 'scripts/get-cdrs', 'scripts/get-did-info', 'scripts/send-sms-message', 'scripts/write-phone-CDR-records' ],
     author              = "RJ White",
-    author_email        = "rj@moxad.com",
+    author_email        = "rj.white@moxad.com",
     maintainer          = "RJ White",
-    maintainer_email    = "rj@moxad.com",
+    maintainer_email    = "rj.white@moxad.com",
     url                 = "http://github.com/rjwhite/Python-voip.ms",
     description         = "command-line scripts to use voip.ms API",
     long_description    = "command-line scripts to use voip.ms API",
